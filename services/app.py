0bf63a1931e40e2be569bc383441fbdd8287fad1
Amit M <amitkmj78@gmail.com>
2025-12-26 18:28:20 -0500

    remove ta

--- services/app.py
+++ services/app.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
-import ta
+
 import streamlit as st
 
 # -------------------------------------------------------------
